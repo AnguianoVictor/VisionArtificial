03341ff4087e57ba07948f0a5e3d3460f0662837
AnguianoVictor <vicmaframirez@gmail.com>
2019-02-26 21:37:11 -0600

    SistemaCompleto  20190226

--- ReconocimientoImagenes/ImagenHu.m
+++ ReconocimientoImagenes/ImagenHu.m
@@ -1,13 +1,13 @@
-clc, clear, close all;
+function [ PP ] = ImagenHu( ruta )
 
-%5 imagenes por pokemon. 
+%Este programa obtiene los 7 momentos invariantes de la imagen asociada a
+%la ruta
+k = 1;
+im = imread(ruta);                  %Lectura de imagen
+gris = double(rgb2gray(im));        %Conversion a escala de grises
+[filas, columnas] = size(gris);     %Dimensionamiento de la matriz gris
 
-num_im = 25;
-for k=1: num_im
-im = imread(strcat('.\Modelos\',num2str(k),'.bmp'));
-gris = double(rgb2gray(im));
-[filas, columnas] = size(gris);
-
+%Algoritmo de binarizacion de imagen. 
 for i =1: filas
     for j=1: columnas
         if (gris(i,j) <= 240)
@@ -18,8 +18,10 @@ for i =1: filas
     end
 end
 
+%Formacion de matrices respecto al dimensionamiento de la imagen inicial
 [Y,X] = meshgrid(1:filas,1:columnas);
 
+% Calculo de los 7 momentos de HU
 for i=1:4
     for j=1:4
          m(i,j) = PrimerMomento(X,Y,bin,i-1,j-1);
@@ -51,8 +53,15 @@ phi(k,6) = (n(dos,cero) - n(cero,dos))*((n(tres,cero) + n(uno,dos)).^2 - (n(dos,
     + 4*n(uno,uno)*((n(tres,cero) + n(uno,dos))*(n(dos,uno) + n(cero,tres)));
 phi(k,7) = (3*n(dos,uno) - n(cero,tres))*(n(tres,cero) + n(uno,dos))*((n(tres,cero) + n(uno,dos)).^2 - 3*(n(dos,uno) + n(cero,tres)).^2)...
     + (3*n(uno,dos) - n(tres,cero)*(n(dos,uno) + n(cero,tres)))*(3*(n(tres,cero) + n(uno,dos)).^2 - (n(dos,uno) + n(cero,tres).^2));
+
+
+% sec0 = phi((k*0)+1:k*1,1:7);
+% sec1 = phi(k+1:k*2,1:7);
+% sec2 = phi((k*2)+1:k*3,1:7);
+% sec3 = phi((k*3)+1:k*4,1:7);
+% sec4 = phi((k*4)+1:k*5,1:7);
+
+PP = phi';
+
 end
 
-P = phi';
-
-save invariantes P
---
+++ ReconocimientoImagenes/Reconocimiento.m
@@ -0,0 +1,55 @@
+clc, clear, close all;
+
+% Reconocimiento de imagenes a traves de una red neuronal Competitiva
+
+load('invariantes.mat')                 %Lectura de momentos invariantes de las 25 imagenes de entrenamiento creados por el script MomentosHu.m
+[NE, NP] = size(P);                     %Se obtiene el numero de elementos, en este caso 7 momentos invariantes
+                                        %NP se refiere al numero de
+                                        %entradas, en este caso 25, que
+                                        %corresponden a las 25 imagenes de
+                                        %entrenamiento
+for i=1:NP
+    W(:,i) = P(:,i)./norm(P(:,i));      %Normalizar los vectores de entrada para convertir en vectores unitarios
+                                        %y guardarlos como los pesos
+                                        %sinapticos
+end
+
+imagenPrueba = '.\Comprobacion\Mewtwo.bmp';
+PP = ImagenHu(imagenPrueba);%Aqui se coloca la ruta de la imagen a reconocer
+PP=PP./norm(PP);                        %Se normalizan los momentos invariantes de la imagen a reconocer
+
+% Se colocan como parametros los pesos sinapticos y la imagen a reconcoer
+% en el comando compet, que corresponde a una red neuronal competitiva
+a = compet(W'*PP);
+[Y,I] = max(a);                         %Se obtiene el valor máximo que correspondera a 
+                                        %la neurona que se activo. 
+
+% Valores de las neuronas asociadas a cada imagen reconocida. 
+B = [1,6,11,16,21];
+C = [2,7,12,17,22];
+S = [3,8,13,18,23];
+M = [4,9,14,19,24];
+MT= [5,10,15,20,25];
+
+%Algoritmo para mostrar el nombre de la imagen reconocida. 
+for j = 1:NP/5
+    if I == B(j)
+        salida = 'Bulbasur';
+    end
+    if I == C(j)
+        salida = 'Charmander';
+    end
+    if I == S(j)
+        salida = 'Squirtle';
+    end
+    if I == M(j)
+        salida = 'Meow';
+    end
+    if I == MT(j)
+        salida = 'Meowtwo';
+    end
+end
+
+disp(['La imagen reconocida corresponde a un ', salida]);
+imshow(imread(imagenPrueba));
+title(salida)
